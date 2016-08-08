04c9273556e68ec923955515a73f6c5c56f0e0cb
EntranceJew <EntranceJew@gmail.com>
2016-08-08 16:50:59 -0400

    added dictation scanner to Poggeychew scene, dear lord tom committed all the files he shouldn't have

---
+++ Assets/Scripts/SpeechDictation.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Windows.Speech;
+
+public class SpeechDictation : MonoBehaviour {
+    [SerializeField]
+    private string m_Hypotheses;
+
+    [SerializeField]
+    private string m_Recognitions;
+
+    private DictationRecognizer m_DictationRecognizer;
+
+    void Start() {
+        m_DictationRecognizer = new DictationRecognizer();
+
+        m_DictationRecognizer.DictationResult += (text, confidence) =>
+        {
+            Debug.LogFormat("Dictation result: {0}", text);
+            m_Recognitions += text + "\n";
+        };
+
+        m_DictationRecognizer.DictationHypothesis += (text) =>
+        {
+            Debug.LogFormat("Dictation hypothesis: {0}", text);
+            m_Hypotheses += text;
+        };
+
+        m_DictationRecognizer.DictationComplete += (completionCause) =>
+        {
+            if (completionCause != DictationCompletionCause.Complete)
+                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+        };
+
+        m_DictationRecognizer.DictationError += (error, hresult) =>
+        {
+            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+        };
+
+        m_DictationRecognizer.Start();
+    }
+}
